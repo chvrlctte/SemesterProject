750f7b49446e0d8c00d2f186ce5b4de1f9dfa4b9
chvrlctte <113151203+chvrlctte@users.noreply.github.com>
2022-12-11 16:15:59 -0500

    First attempt at making image clickable

---
+++ main.js
@@ -0,0 +1,11 @@
+const myArray = [s13, ae86, miata];
+
+image = document.querySelector('img');
+image.addEventListener("click", function () {
+  window.location.href = this.s13 + './s13.html';
+});
+
+image = document.querySelector('img');
+image.addEventListener("click", function () {
+  window.location.href = this.ae86 + './ae86.html';
+});
